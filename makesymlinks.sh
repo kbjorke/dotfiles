703e79f75137862ae39273090798d7b6b2a5551a
Kristian Bjørke <kbjorke@gmail.com>
2014-10-18 16:06:33 +0100

    Added dir_colors directory to made by install script.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -12,8 +12,8 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vim vimrc" # list of files/folders to symlink in homedir
-
+# list of files/folders to symlink in homedir:
+files="bashrc vim vimrc dir_colors" 
 ##########
 
 # create dotfiles_old in homedir
